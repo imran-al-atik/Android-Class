ab086f44392517d125d403b63ed33f0a3ae6ac82
imran-al-atik <58503257+imran-al-atik@users.noreply.github.com>
2019-12-04 13:14:26 +0600

    Create Human.java
    My solution to problem interface.

---
+++ Human.java
@@ -0,0 +1,28 @@
+interface Human {
+public String charecter () ;
+
+
+
+public int age () ;
+public String partner () ;
+
+}
+interface mama {
+
+
+}
+
+public class Man implements human {
+public String charecter () {
+ return "Honest and Rare" ;
+}
+
+public int age () {
+	int realage = 50;
+	return realage = 5;
+}
+public String partner () {
+	
+	return "Not Found";
+}
+}
